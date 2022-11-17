94f4d997e917953b1ad376a8361319d22399fed9
bfranceschin <bfranceschin@gmail.com>
2022-11-17 19:31:27 -0300

    refactor: explore page with real data

--- packages/frontend/components/explore/CardPaper.js
+++ packages/frontend/components/explore/CardPaper.js
@@ -1,43 +1,52 @@
+
+
+import {
+  useTokenMetaData,
+  useTokenImage
+} from "../../hooks/nft"
+
+import {
+  ipfsToHTTP,
+} from '../../utils'
+
 const ImageIPFS = (imgUrl) => {
   return (
     <div></div>
   )
 }
 
-const CardPaper = (props) => {
+const CardPaper = ({tokenId, metadata}) => {
+  let imageUrl = metadata ? ipfsToHTTP(metadata.image) : null
+  const href = `/nfts/${tokenId}`
   return ( 
     <div className="card w-96 bg-base-100 shadow-xl mb-5">
       <div className="h-64 bg-base-300 place-content-center">
-        {props.cover !== "" 
-          ? <ImageIPFS imgUrl={props.cover}/>
-          : <figure className="h-full overflow-hidden">
-              <img width="384px" height="100px" src={imageTempURL} />
-            </figure>
-        }
-        
+        <figure className="h-full overflow-hidden">
+          <img width="384px" height="100px" src={imageUrl}/>
+        </figure>
       </div>
       <div className="card-body">
         <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
           <a
-            href="/"
+            href={href}
             className="transition-colors duration-200 text-gray-900 hover:text-purple-700"
             aria-label="Category"
             title="traveling"
           >
-            {props.author}
+            {metadata ? metadata.properties.author : ""}
           </a>
-          <span className="text-gray-600"> {props.date} </span>
+          {/* <span className="text-gray-600"> {props.date} </span> */}
         </p>
         <a
-          href="/"
+          href={href}
           aria-label="Category"
           title="Visit the East"
           className="inline-block mb-3 text-2xl font-bold leading-5 transition-colors duration-200 hover:text-purple-700"
         >
-          {props.title}
+          {metadata ? metadata.name : ""}
         </a>
         <p className="mb-2 text-gray-700">
-          {props.keywords}
+          {metadata ? metadata.properties.keywords : ""}
         </p>
       </div>
     </div>
--- packages/frontend/pages/explore.js
+++ packages/frontend/pages/explore.js
@@ -1,8 +1,21 @@
+import { useEffect, useState } from "react";
 import Navbar from '../components/Navbar';
 import SearchBar from '../components/explore/SearchBar';
 import CardPaper from '../components/explore/CardPaper';
+import Link from "next/link";
+
+import { useAllMetadata } from '../hooks/nft'
+
+const processSearch = (searchCategory, searchFrase, allMetadata) => {
+  return allMetadata
+}
 
 export default function ExploreComponent() {
+  const [searchCategory, setSearchCategory] = useState('')
+  const [searchFrase, setSearchFrase] = useState('')
+  const allMetadata = useAllMetadata()
+  const searchList = processSearch(searchCategory, searchFrase, allMetadata)
+  // console.log("searchList",searchList)
   return (
     <div>
       <Navbar/>
@@ -13,14 +26,13 @@ export default function ExploreComponent() {
         <div className="p-4 place-items-center">  
           {/** width 96 is the same as CardPaper component */}
           <div className="flex flex-wrap flex-none flex-row justify-between gap-6 p-4">
-            <CardPaper />
-            <CardPaper />
-            <CardPaper />
-            <CardPaper />
-            <CardPaper />
-            <CardPaper />
-            <CardPaper />
-            <CardPaper />
+            {
+              searchList.map(e => {
+                return (
+                  <CardPaper tokenId={e.tokenId} metadata={e.metadata}/>
+                )
+              })
+            }
             {/** Important: do not remove the next 3 rows! */}
             <div className="w-96 h-0" />
             <div className="w-96 h-0" />
