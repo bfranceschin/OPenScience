89df0d83f8fcff649f35c1afce955bf327b02ab9
bfranceschin <bfranceschin@gmail.com>
2022-11-21 18:08:56 -0300

    add initial nft for testing

--- packages/backend/deploy/00_deploy_contract.js
+++ packages/backend/deploy/00_deploy_contract.js
@@ -11,5 +11,17 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     from: deployer,
     log: true,
   });
+  
+  
+  const chainId = await getChainId();
+  if (chainId === '420') { //goerliOptimism
+    // Add initial nfts for testing purposes
+    const {owner} = await getNamedAccounts();
+    const contract = await ethers.getContract('NFT', owner);
+    const bitcoinUri = "ipfs://bafyreihkbv53y7e3ii6miqilo2ax6sn6rjqx3bbs2pngf5b7gx7bzoyada/metadata.json"
+    const tx = await contract.createToken(bitcoinUri, [])
+    console.log("Adding bitcoin paper: tx ", tx.hash)
+    const txReceipt = await tx.wait()
+  }
 };
 module.exports.tags = ['all', 'NFT'];
