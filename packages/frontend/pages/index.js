5970f0c827c56fe6ff2fb5c1a43b0409d3e8c94a
bfranceschin <bfranceschin@gmail.com>
2022-11-29 11:54:28 -0300

    refactor: landing page components

--- packages/frontend/pages/index.js
+++ packages/frontend/pages/index.js
@@ -96,8 +96,7 @@ const Content1_2 = () => {
           Open Science <span className="text-[#767B91]">for All</span>
         </h2>
         <p className="text-base text-gray-700 md:text-lg sm:px-4">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque rem aperiam, eaque ipsa quae.
+          Open access. Permissionless publishing. Graph-Funding.
         </p>
       </div>
     </div>
@@ -112,11 +111,11 @@ export const Content2 = () => {
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="mx-auto sm:text-center lg:max-w-2xl">
         <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-          <div>
+          {/* <div>
             <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
               Brand new
             </p>
-          </div>
+          </div> */}
           <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
             <span className="relative inline-block">
               <svg
@@ -143,11 +142,10 @@ export const Content2 = () => {
               </svg>
               <span className="relative">The</span>
             </span>{' '}
-            quick, brown fox jumps over a lazy dog
+            Mission
           </h2>
           <p className="text-base text-gray-700 md:text-lg">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque rem aperiam, eaque ipsa quae.
+            The Open Science Project's mission is to make access to scientific information free and accessible to everyone while enabling scientists to receive funding for their endeavors.
           </p>
         </div>
         <div className="mb-4 transition-shadow duration-300 hover:shadow-xl lg:mb-6">
@@ -368,14 +366,155 @@ export const Content3 = () => {
 };
 
 
+const Feature = () => {
+  return (
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+        <div>
+          <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+            Brand new
+          </p>
+        </div>
+        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          <span className="relative inline-block">
+            <svg
+              viewBox="0 0 52 24"
+              fill="currentColor"
+              className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+            >
+              <defs>
+                <pattern
+                  id="18302e52-9e2a-4c8e-9550-0cbb21b38e55"
+                  x="0"
+                  y="0"
+                  width=".135"
+                  height=".30"
+                >
+                  <circle cx="1" cy="1" r=".7" />
+                </pattern>
+              </defs>
+              <rect
+                fill="url(#18302e52-9e2a-4c8e-9550-0cbb21b38e55)"
+                width="52"
+                height="24"
+              />
+            </svg>
+            <span className="relative">Take</span>
+          </span>{' '}
+          part in the open science revolution
+        </h2>
+        {/* <p className="text-base text-gray-700 md:text-lg">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque rem aperiam, eaque ipsa quae.
+        </p> */}
+      </div>
+      <div className="grid gap-4 row-gap-5 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+          <div>
+            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              <svg
+                className="w-12 h-12 text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5">Open access</h6>
+            <p className="mb-3 text-sm text-gray-900">
+               Don't get stuck behind paywalls owned by a few big platforms. Explore all the latest published research.
+            </p>
+          </div>
+          <a
+            href="/"
+            aria-label=""
+            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+          >
+            Learn more
+          </a>
+        </div>
+        <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+          <div>
+            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              <svg
+                className="w-12 h-12 text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5">When has justice</h6>
+            <p className="mb-3 text-sm text-gray-900">
+              Rough pomfret lemon shark plownose chimaera southern sandfish
+              kokanee northern sea.
+            </p>
+          </div>
+          <a
+            href="/"
+            aria-label=""
+            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+          >
+            Learn more
+          </a>
+        </div>
+        <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+          <div>
+            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              <svg
+                className="w-12 h-12 text-deep-purple-accent-400"
+                stroke="currentColor"
+                viewBox="0 0 52 52"
+              >
+                <polygon
+                  strokeWidth="3"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  fill="none"
+                  points="29 13 14 29 25 29 23 39 38 23 27 23"
+                />
+              </svg>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5">Organically grow</h6>
+            <p className="mb-3 text-sm text-gray-900">
+              A slice of heaven. O for awesome, this chocka full cuzzie is as
+              rip-off as a cracker.
+            </p>
+          </div>
+          <a
+            href="/"
+            aria-label=""
+            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+          >
+            Learn more
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 
 const LandingPageComponent = () => {
   return (
     <div>
       <NavbarLandPage/>   
       <Content1_2 />
-      <Content2 />
-      <Content3 />
+      <Feature />
+      {/* <Content2 />
+      <Content3 /> */}
     </div>
   )
 }
