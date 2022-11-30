e013ab9c72418de8d9aa662671943d02d588aeb2
bfranceschin <bfranceschin@gmail.com>
2022-11-30 18:55:39 -0300

    feat: team section

--- packages/frontend/pages/index.js
+++ packages/frontend/pages/index.js
@@ -536,6 +536,58 @@ const Feature = () => {
   );
 };
 
+const Team = () => {
+  return (
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-10 mx-auto sm:grid-cols-2 lg:grid-cols-3 lg:max-w-screen-lg">
+        <div>
+          <img
+            className="object-cover w-24 h-24 rounded-full shadow"
+            // src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+            src="https://avatars.githubusercontent.com/u/50725001?v=4;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
+            alt="Person"
+          />
+          <div className="flex flex-col justify-center mt-2">
+            <p className="text-lg font-bold">Bernardo Franceschin</p>
+            <p className="mb-4 text-xs text-gray-800">Fullstack / smart contracts</p>
+            <p className="text-sm tracking-wide text-gray-800">
+              bfranceschin@gmail.com
+            </p>
+          </div>
+        </div>
+        <div>
+          <img
+            className="object-cover w-24 h-24 rounded-full shadow"
+            src="https://avatars.githubusercontent.com/u/16672082?v=4;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+            alt="Person"
+          />
+          <div className="flex flex-col justify-center mt-2">
+            <p className="text-lg font-bold">Fernanda Duarte</p>
+            <p className="mb-4 text-xs text-gray-800">Frontend</p>
+            <p className="text-sm tracking-wide text-gray-800">
+              fcduarte26@gmail.com
+            </p>
+          </div>
+        </div>
+        <div>
+          <img
+            className="object-cover w-24 h-24 rounded-full shadow"
+            src="https://avatars.githubusercontent.com/u/103785545?v=4;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+            alt="Person"
+          />
+          <div className="flex flex-col justify-center mt-2">
+            <p className="text-lg font-bold">Pedro Franceschin</p>
+            <p className="mb-4 text-xs text-gray-800">Smart contracts</p>
+            <p className="text-sm tracking-wide text-gray-800">
+              pbfranceschin@gmail.com
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 
 const LandingPageComponent = () => {
   return (
@@ -543,6 +595,7 @@ const LandingPageComponent = () => {
       <NavbarLandPage/>   
       <Content1_2 />
       <Feature />
+      <Team />
       {/* <Content2 />
       <Content3 /> */}
     </div>
